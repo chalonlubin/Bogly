c0d5823125f0c1961b793f9beb2498bbcb1a6834
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2022-12-08 13:34:32 -0800

    add created_at

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
-
 db = SQLAlchemy()
+
+from datetime import datetime
 
 DEFAULT_IMAGE_URL = 'https://ichef.bbci.co.uk/news/976/cpsprodpb/3497/production/_108636431_one.jpg.webp'
 
@@ -26,6 +26,9 @@ class User(db.Model):
     image_url = db.Column(db.Text, nullable=True,
                           default=DEFAULT_IMAGE_URL)
 
+    def __repr__(self):
+        return f"<User {self.first_name} {self.last_name} id={self.id}>"
+
 
 class Post(db.Model):
     """Post"""
@@ -35,11 +38,29 @@ class Post(db.Model):
     id = db.Column(db.Integer,
          primary_key=True,
          autoincrement=True)
-    title = db.Column(db.String(150), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    # created_at = db.Column(db.DateTime, server_default=db.func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(
+        db.String(150),
+        nullable=False)
+    content = db.Column(
+        db.Text,
+        nullable=False)
 
-    author = db.relationship("User", backref='posts')
+    # ProgrammingError: (psycopg2.ProgrammingError) can't adapt type 'now'
+    # [SQL: INSERT INTO posts (title, content, created_at, user_id) VALUES (%(title)s, %(content)s, %(created_at)s, %(user_id)s) RETURNING posts.id]
+    # [parameters: {'title': 'My first post', 'content': 'Hello', 'created_at': <sqlalchemy.sql.functions.now at 0x7fcb78a8d840; now>, 'user_id': 1}]
+    # (Background on this error at: https://sqlalche.me/e/14/f405)
+    # created_at = db.Column(
+    #     db.DateTime,
+    #     nullable=False,
+    #     default=db.func.now)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'))
+
+    # post instance .author --> user
+    author = db.relationship(
+        "User",
+        backref='posts')
 
 
