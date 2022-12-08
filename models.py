7ad1aaafe3cbe12a41520ac9e854a6de72956b6d
Chalon Lubin <chalonlubin1@gmail.com>
2022-12-08 12:11:32 -0800

    swap

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
@@ -15,7 +16,7 @@ def connect_db(app):
 class User(db.Model):
     """User"""
 
-    __tablename__ = "Users"
+    __tablename__ = "users"
 
     id = db.Column(db.Integer,
          primary_key=True,
@@ -24,3 +25,21 @@ class User(db.Model):
     last_name = db.Column(db.String(100), nullable=False)
     image_url = db.Column(db.Text, nullable=True,
                           default=DEFAULT_IMAGE_URL)
+
+
+class Post(db.Model):
+    """Post"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+         primary_key=True,
+         autoincrement=True)
+    title = db.Column(db.String(150), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    # created_at = db.Column(db.DateTime, server_default=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    author = db.relationship("User", backref='posts')
+
+
