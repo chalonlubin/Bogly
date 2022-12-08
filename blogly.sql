4f579c5066cbdfa48e911cae9d900f7e1e5b7695
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2022-12-07 17:31:14 -0800

    dump db

--- blogly.sql
+++ blogly.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.5 (Homebrew)
--- Dumped by pg_dump version 14.5 (Homebrew)
+-- Dumped from database version 14.5 (Ubuntu 14.5-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.5 (Ubuntu 14.5-0ubuntu0.22.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -16,12 +16,16 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+ALTER TABLE ONLY public."Users" DROP CONSTRAINT "Users_pkey";
+ALTER TABLE public."Users" ALTER COLUMN id DROP DEFAULT;
+DROP SEQUENCE public."Users_id_seq";
+DROP TABLE public."Users";
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
 --
--- Name: Users; Type: TABLE; Schema: public; Owner: Chalon
+-- Name: Users; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public."Users" (
@@ -32,10 +36,8 @@ CREATE TABLE public."Users" (
 );
 
 
-ALTER TABLE public."Users" OWNER TO "Chalon";
-
 --
--- Name: Users_id_seq; Type: SEQUENCE; Schema: public; Owner: Chalon
+-- Name: Users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE public."Users_id_seq"
@@ -47,43 +49,38 @@ CREATE SEQUENCE public."Users_id_seq"
     CACHE 1;
 
 
-ALTER TABLE public."Users_id_seq" OWNER TO "Chalon";
-
 --
--- Name: Users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Chalon
+-- Name: Users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public."Users_id_seq" OWNED BY public."Users".id;
 
 
 --
--- Name: Users id; Type: DEFAULT; Schema: public; Owner: Chalon
+-- Name: Users id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public."Users" ALTER COLUMN id SET DEFAULT nextval('public."Users_id_seq"'::regclass);
 
 
 --
--- Data for Name: Users; Type: TABLE DATA; Schema: public; Owner: Chalon
+-- Data for Name: Users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
 COPY public."Users" (id, first_name, last_name, image_url) FROM stdin;
-1	Kadeem	Best	https://ichef.bbci.co.uk/news/976/cpsprodpb/3497/production/_108636431_one.jpg.webp
-2	Sirius	Black	https://www.ethosvet.com/wp-content/uploads/black-cat-625x375.png
-3	George 	Foreman	
-4	George	GrillsALot	https://ichef.bbci.co.uk/news/976/cpsprodpb/3497/production/_108636431_one.jpg.webp
+1	Mike	Wick	https://ichef.bbci.co.uk/news/976/cpsprodpb/3497/production/_108636431_one.jpg.webp
 \.
 
 
 --
--- Name: Users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Chalon
+-- Name: Users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public."Users_id_seq"', 4, true);
+SELECT pg_catalog.setval('public."Users_id_seq"', 2, true);
 
 
 --
--- Name: Users Users_pkey; Type: CONSTRAINT; Schema: public; Owner: Chalon
+-- Name: Users Users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public."Users"
